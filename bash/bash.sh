1f985bbaf1319d2ca8dab69905c059004fd708fc
Jackie Purdue <jpurdue@dal.ca>
2019-04-26 14:33:04 -0300

    Update bash configuration file

--- bash/bash.sh
+++ bash/bash.sh
@@ -1,101 +1,117 @@
-#
-# If not running interactively, don't do anything
-case $- in
-    *i*) ;;
-      *) return;;
-esac
+_main() {
+  export PATH=~/.local/bin:$PATH
+  _history
+  _prompt
+  unset _main
+}
 
-HISTCONTROL=ignoreboth
+_prompt() {
+  export PS1="\[\e[38;5;58m\]$ \[\e[0m\]"
+  export PS2="\[\e[38;5;58m\]> \[\e[0m\]"
+  unset _prompt
+}
 
-# append to the history file, don't overwrite it
-shopt -s histappend
+_history() {
+  export HISTTIMEFORMAT="%h %d %H:%M:%S"
+  export HISTFILESIZE=40000
+  export HISTSIZE=10000
+  export HISTCONTROL=ignoredups
+  export HISTIGNORE="&:ls:[bf]g:exit:history"
+  shopt -s histappend
+  shopt -s cmdhist
+  unset _history
+}
 
-# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
-
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
-shopt -s checkwinsize
-
-# If set, the pattern "**" used in a pathname expansion context will
-# match all files and zero or more directories and subdirectories.
-#shopt -s globstar
-
-# make less more friendly for non-text input files, see lesspipe(1)
-[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
-
-export PS1="\[\e[38;5;58m\]$ \[\e[0m\]"
+l() {
+  ls
+}
 
 
+ll() {
+  ls -l
+}
 
-# enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
 
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-fi
+la() {
+  ls -a
+}
 
-# colored GCC warnings and errors
-#export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
+lla() {
+  ls -l -a
+}
 
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+cl() {
+  clear
+  ls
+}
 
-# Alias definitions.
-# You may want to put all your additions into a separate file like
-# ~/.bash_aliases, instead of adding them here directly.
-# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+cll() {
+  clear
+  ls -l
+}
 
-if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
-fi
+clla() {
+  clear
+  ls -l -a
+}
 
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
+h() {
+  cd $HOME
+}
+
+r() {
+  cd /
+}
+
+dt() {
+  date "+%F %T"
+}
+
+extract () {
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)
+        tar xvjf $1
+	;;
+      *.tar.gz)
+        tar xvzf $1
+	;;
+      *.bz2)
+        bunzip2 $1
+        ;;
+      *.rar)
+        unrar x $1
+        ;;
+      *.gz)
+        gunzip $1
+        ;;
+      *.tar)
+        tar xvf $1
+        ;;
+      *.tbz2)
+       tar xvjf $1
+       ;;
+      *.tgz)
+        tar xvzf $1
+        ;;
+      *.zip)
+        unzip $1
+        ;;
+      *.Z)
+        uncompress $1
+        ;;
+      *.7z)
+        7z x $1
+        ;;
+      *)
+        echo -e "Error: the extension of the file '$1' is not in the list of" \
+	        "compressed file types." 1>&2
+        ;;
+    esac
+  else
+    echo "Error: the file '$1' is not valid." 1&2
   fi
-fi
+}
 
-
-# added by Anaconda3 5.3.1 installer
-# >>> conda init >>>
-# !! Contents within this block are managed by 'conda init' !!
-#__conda_setup="$(CONDA_REPORT_ERRORS=false '/home/jax/.anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
-#if [ $? -eq 0 ]; then
-#    \eval "$__conda_setup"
-#else
-#    if [ -f "/home/jax/.anaconda3/etc/profile.d/conda.sh" ]; then
-#        . "/home/jax/.anaconda3/etc/profile.d/conda.sh"
-#        CONDA_CHANGEPS1=false conda activate base
-#    else
-#        \export PATH="/home/jax/.anaconda3/bin:$PATH"
-#    fi
-#fi
-#unset __conda_setup
-
-export PATH=/home/jax/.local/bin:$PATH
-export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
-#PROMPT_COMMAND='history -a'
-
+_main
